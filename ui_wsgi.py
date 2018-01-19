5585a1ab82160a467d4da9aae1db79a7ee942f36
Josh Nisly <github@joshnisly.com>
2018-01-19 18:58:42 -0500

    Start work on UI.

---
+++ ui_wsgi.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import flask
+
+import game
+import game_cards
+
+app = flask.Flask(__name__)
+
+THE_GAME = game.SplendorGame()
+THE_GAME.init_random()
+THE_GAME.add_player(lambda: [], None)
+THE_GAME.add_player(lambda: [], None)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    game_state = THE_GAME.get_game_state()
+    return flask.render_template('index.html', **{
+        'state': game_state,
+        'colors': game_cards.COLOR_VALUES
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
